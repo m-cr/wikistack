b7e65c955230b16ca4fa5f3288d67ed9210d5a0e
Mike <miketcrowley@gmail.com>
2016-08-11 00:21:24 +0000

    stuck at sequalize instantiation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var swig = require('swig');
 swig.setDefaults({ cache:false });
+var models = require('./models')
 
 var app = express();
 
@@ -17,6 +18,13 @@ app.get('/', function(req, res, next){
 	res.render('index', {});
 });
 
-app.listen(process.env.PORT, function(){
-	console.log('listening on port ' + process.env.PORT);
-});
+models.User.sync({})
+.then(function(){
+	return models.Page.sync({})
+}).then(function(){
+	app.listen(process.env.PORT, function(){
+		console.log('listening on port ' + process.env.PORT);
+	});
+})
+.catch(console.error);
+
--- models/index.js
+++ models/index.js
@@ -1,2 +1,44 @@
 var Sequelize = require('sequelize');
+// var postgresURI = 'postgres://localhost:5432/wikistack';
+// var db = new Sequelize('wikistack', 'ubuntu', 'cloud9',{ host: 'localhost', dialect: 'postgres'}, {logging:false});
 var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    }
+});
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        isEmail: true,
+        allowNull: false
+    }
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+};
+
